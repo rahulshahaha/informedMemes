ccfece24ba0ceecd525585e55de17b0873d55fe7
rahulshahaha <getrshah@gmail.com>
2020-08-15 12:58:55 -0400

    y

--- informed-memes/src/App.js
+++ informed-memes/src/App.js
@@ -9,7 +9,11 @@ class App extends React.Component {
 
   render(){
       return (
-        <h1>Test</h1>
+        <div>
+          <section id="photos">
+            <a href="https://www.theverge.com/2020/8/14/21368651/apple-fortnite-ios-app-store-ban-lawsuit-epic-games-payments"><img src="images/fortnite.png" alt=""></img></a>
+          </section>
+      </div>
       );
     }
   }
