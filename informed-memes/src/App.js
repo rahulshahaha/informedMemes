a18f39b94c10b59ee96f0ca8751ec69cb27026da
rahulshahaha <getrshah@gmail.com>
2020-08-15 10:31:36 -0400

    Initial setup

---
+++ informed-memes/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+
+class App extends React.Component {
+
+
+
+  render(){
+      return (
+        <h1>Test</h1>
+      );
+    }
+  }
+  
+  
+
+export default App;
