1a63fc1acdf4d84b41ad9ddeba4d76a02dff1c18
rahulshahaha <getrshah@gmail.com>
2020-08-15 12:42:00 -0400

    Updates

---
+++ precache-manifest.3da23026d29a8f37c1270f214dca939c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2ec0d7d40ab92cb58ba9018ceb2cf2",
+    "url": "/informedMemes/index.html"
+  },
+  {
+    "revision": "a5aa8d9b743883a5038a",
+    "url": "/informedMemes/static/css/main.81432683.chunk.css"
+  },
+  {
+    "revision": "16f06d310f7f0e3d0d5d",
+    "url": "/informedMemes/static/js/2.36fc98b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/informedMemes/static/js/2.36fc98b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5aa8d9b743883a5038a",
+    "url": "/informedMemes/static/js/main.5178fcf3.chunk.js"
+  },
+  {
+    "revision": "c7979e307fd114bd99ae",
+    "url": "/informedMemes/static/js/runtime-main.2e0fa6be.js"
+  }
+]);
