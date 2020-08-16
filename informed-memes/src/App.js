2c93eddd5058ed02b12e23940f7774aee1cbc706
rahulshahaha <getrshah@gmail.com>
2020-08-16 12:04:28 -0400

    postmaster post

--- informed-memes/src/App.js
+++ informed-memes/src/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
       return (
         <div>
           <section id="photos">
+            <a href="https://www.bbc.com/news/world-us-canada-53795876"><img src="images/postmaster.png" alt=""></img></a>
             <a href="https://www.theverge.com/2020/8/14/21368651/apple-fortnite-ios-app-store-ban-lawsuit-epic-games-payments"><img src="images/fortnite.png" alt=""></img></a>
           </section>
       </div>
