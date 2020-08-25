1c79231f288f755a107f8c56a4020be7e5da9899
rahulshahaha <getrshah@gmail.com>
2020-08-24 20:10:36 -0400

    federalaid

--- informed-memes/src/App.js
+++ informed-memes/src/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
       return (
         <div>
           <section id="photos">
+            <a href="https://apnews.com/1dc19d5c9d6345e7341d289d2f935b40"><img src="https://i.imgflip.com/4cj0q2.jpg" alt=""></img></a>
+            <a href="https://www.wsj.com/articles/tesla-owners-can-find-their-electric-flame-11597939040"><img src="images/tesla.jpg" alt=""></img></a>
+            <a href="https://apnews.com/33228796a8a66a50546c9e3ddacfef1b"><img src="images/federalaid.jpeg" alt=""></img></a>
             <a href="https://apnews.com/3b898c7b6852af9de09617381dc3f540"><img src="images/israel.jpg" alt=""></img></a>
             <a href="https://www.bbc.com/news/world-us-canada-53795876"><img src="images/postmaster.png" alt=""></img></a>
             <a href="https://www.theverge.com/2020/8/14/21368651/apple-fortnite-ios-app-store-ban-lawsuit-epic-games-payments"><img src="images/fortnite.png" alt=""></img></a>
